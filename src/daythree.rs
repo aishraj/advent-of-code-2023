50c4a985e1639a3b0db7d097eaf5889516b4be3f
Aish <aish@aishraj.com>
2023-12-03 09:59:00 -0800

    Code cleanup

--- src/daythree.rs
+++ src/daythree.rs
@@ -1,5 +1,5 @@
 use std::{
-    collections::{HashMap, HashSet, VecDeque},
+    collections::{HashSet, VecDeque},
     str::FromStr,
 };
 
@@ -39,7 +39,6 @@ impl Graph {
         let mut numbers = vec![];
         queue.push_back(start);
         while let Some((i, j)) = queue.pop_front() {
-            let c = self.0[i][j];
             visited.insert((i, j));
             for (dx, dy) in directions.iter() {
                 let nx = i as i32 + dx;
@@ -61,8 +60,6 @@ impl Graph {
                 }
             }
         }
-        //println!("visited: {:?}", visited);
-        // For every visited, extract the number by rowise scanning
         for (i, row) in self.0.iter().enumerate() {
             let mut current_number = String::new();
             for (j, c) in row.iter().enumerate() {
@@ -80,7 +77,7 @@ impl Graph {
         return numbers;
     }
 
-    pub fn get_symbol_pos(&self, symbol: char) -> Vec<(usize, usize)> {
+    fn get_symbol_pos(&self, symbol: char) -> Vec<(usize, usize)> {
         let mut result = vec![];
         for (i, row) in self.0.iter().enumerate() {
             for (j, c) in row.iter().enumerate() {
@@ -93,92 +90,30 @@ impl Graph {
         return result;
     }
 
-    pub fn search(&self) -> Graph {
+    pub fn search(&self) -> Vec<u32> {
         // we do a search from all symbols and mark any numbers we find as 'visited'
-        let mut output = self.0.clone();
-        let mut queue = VecDeque::new();
-        let mut visited = HashSet::new();
+        let mut symbols = vec![];
 
         for (i, row) in self.0.iter().enumerate() {
             for (j, c) in row.iter().enumerate() {
                 if *c != '.' && !c.is_numeric() {
-                    queue.push_back((i, j));
+                    symbols.push((i, j));
                 }
             }
         }
 
-        let directions = vec![
-            (0, 1),
-            (0, -1),
-            (1, 0),
-            (-1, 0),
-            (1, 1),
-            (-1, -1),
-            (1, -1),
-            (-1, 1),
-        ];
-        while let Some((i, j)) = queue.pop_front() {
-            let c = self.0[i][j];
-            println!("x y c - {} {} {}", i, j, c);
-            visited.insert((i, j));
-            for (dx, dy) in directions.iter() {
-                let nx = i as i32 + dx;
-                let ny = j as i32 + dy;
-                if nx < 0 || ny < 0 {
-                    continue;
-                }
-                let nx = nx as usize;
-                let ny = ny as usize;
-                if ny >= self.0[0].len() || nx >= self.0.len() {
-                    continue;
-                }
-                let c = self.0[nx][ny];
-                if c.is_numeric() || (!c.is_numeric() && c != '.') {
-                    if visited.contains(&(nx, ny)) {
-                        continue;
-                    }
-                    println!("nx: {} ny: {} c: {}", nx, ny, c);
-                    queue.push_back((nx, ny));
-                }
-            }
-            println!("\n\n");
+        let mut connected_numbers = vec![];
+        for symbol_pos in symbols.into_iter() {
+            let found_numbers = self.search_numbers_from_position(symbol_pos);
+            connected_numbers.extend(found_numbers);
         }
-
-        for i in 0..self.0.len() {
-            for j in 0..self.0[0].len() {
-                let c = self.0[i][j];
-                if c.is_numeric() && !visited.contains(&(i, j)) {
-                    output[i][j] = '.';
-                }
-            }
-        }
-        return Graph(output);
-    }
-
-    pub fn extract_numbers(&self) -> Vec<u32> {
-        let mut numbers = vec![];
-        for row in self.0.iter() {
-            let mut current_number = String::new();
-            for c in row.iter() {
-                if c.is_numeric() {
-                    current_number.push(*c);
-                } else if !current_number.is_empty() {
-                    numbers.push(current_number.parse::<u32>().unwrap());
-                    current_number = String::new();
-                }
-            }
-            if !current_number.is_empty() {
-                numbers.push(current_number.parse::<u32>().unwrap());
-            }
-        }
-        return numbers;
+        return connected_numbers;
     }
 }
 
 pub fn solve_part_one(input: &str) -> u32 {
     let graph = Graph::from_str(input).unwrap();
-    let graph = graph.search();
-    let mut numbers = graph.extract_numbers();
+    let mut numbers = graph.search();
     numbers.sort();
     println!("The numbers are: {:?}", numbers);
     return numbers.iter().sum();
@@ -198,13 +133,6 @@ pub fn solve_part_two(input: &str) -> u32 {
         sum = sum + prod;
     }
     return sum;
-}
-
-fn parse_input(input: &str) -> Vec<u32> {
-    input
-        .lines()
-        .map(|line| line.parse::<u32>().unwrap())
-        .collect()
 }
 
 #[cfg(test)]
@@ -233,6 +161,6 @@ mod tests {
     #[test]
     fn solves_3_2_hard() {
         let input = std::fs::read_to_string("input/3_real.txt").unwrap();
-        assert_eq!(super::solve_part_two(&input), 467835);
+        assert_eq!(super::solve_part_two(&input), 81721933);
     }
 }

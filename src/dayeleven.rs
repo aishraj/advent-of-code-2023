630838c82a9d3e83d6e0c31cd8a48f868a40497a
Aish <aish@aishraj.com>
2023-12-11 07:01:45 -0800

    11 done - 10 still remains

--- src/dayeleven.rs
+++ src/dayeleven.rs
@@ -1,6 +1,5 @@
-pub fn solve_part_one(input: &str) -> usize {
+pub fn solve(input: &str, expansion: usize) -> usize {
     let mut grid: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
-    expand(&mut grid);
     let mut galaxies = Vec::new();
     for (row, line) in grid.iter().enumerate() {
         for (col, &cell) in line.iter().enumerate() {
@@ -9,22 +8,28 @@ pub fn solve_part_one(input: &str) -> usize {
             }
         }
     }
+
+    expand(&mut grid, &mut galaxies, expansion);
     let mut total = 0;
     for i in 0..galaxies.len() {
         for j in i + 1..galaxies.len() {
             let (r1, c1) = galaxies[i];
             let (r2, c2) = galaxies[j];
-            total += (r1 as i32 - r2 as i32).abs() + (c1 as i32 - c2 as i32).abs();
+            total += (r1 as i64 - r2 as i64).abs() + (c1 as i64 - c2 as i64).abs();
         }
     }
     total as usize
 }
 
-pub fn solve_part_two(input: &str) -> u32 {
-    42
+pub fn solve_part_one(input: &str) -> usize {
+    solve(input, 2)
 }
 
-fn expand(grid: &mut Vec<Vec<char>>) {
+pub fn solve_part_two(input: &str) -> usize {
+    solve(input, 1000000)
+}
+
+fn expand(grid: &mut Vec<Vec<char>>, galaxies: &mut Vec<(usize, usize)>, expansion: usize) {
     let length = grid.len();
     let width = grid[0].len();
 
@@ -47,15 +52,18 @@ fn expand(grid: &mut Vec<Vec<char>>) {
     no_galaxy_row.reverse();
     no_galaxy_col.reverse();
 
-    for &index in &no_galaxy_row {
-        let new_row = vec!['.'; width];
-        grid.insert(index, new_row);
+    for r in no_galaxy_row {
+        for g in &mut *galaxies {
+            if g.0 > r {
+                g.0 += expansion - 1
+            }
+        }
     }
-
-    let new_length = grid.len();
-    for &index in &no_galaxy_col {
-        for row in grid.iter_mut().take(new_length) {
-            row.insert(index, '.');
+    for c in no_galaxy_col {
+        for g in &mut *galaxies {
+            if g.1 > c {
+                g.1 += expansion - 1
+            }
         }
     }
 }
@@ -71,18 +79,18 @@ mod tests {
     #[test]
     fn solves_11_1_hard() {
         let input = std::fs::read_to_string("input/11_hard.txt").unwrap();
-        assert_eq!(super::solve_part_one(&input), 42);
+        assert_eq!(super::solve_part_one(&input), 9312968);
     }
 
     #[test]
     fn solves_11_2_easy() {
         let input = std::fs::read_to_string("input/11_easy.txt").unwrap();
-        assert_eq!(super::solve_part_one(&input), 42);
+        assert_eq!(super::solve(&input, 100), 8410);
     }
 
     #[test]
     fn solves_11_2_hard() {
         let input = std::fs::read_to_string("input/11_hard.txt").unwrap();
-        assert_eq!(super::solve_part_one(&input), 42);
+        assert_eq!(super::solve_part_two(&input), 597714117556);
     }
 }
